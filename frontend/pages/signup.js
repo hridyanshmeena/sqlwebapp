277569017b1a8ff6705cd44cdb831de7bba690b4
hridyanshmeena <43299684+hridyanshmeena@users.noreply.github.com>
2025-10-28 22:18:07 +0530

    Update signup.js

--- frontend/pages/signup.js
+++ frontend/pages/signup.js
@@ -12,7 +12,7 @@ export default function Signup() {
     e.preventDefault();
     setMsg("Signing up...");
     try {
-      const res = await fetch("http://127.0.0.1:8000/api/signup", {
+      const res = await fetch("${process.env.NEXT_PUBLIC_API_BASE}/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
