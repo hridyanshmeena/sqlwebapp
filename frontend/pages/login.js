5cf9b8a5071c0c6b06add55f0eba418363a7fa95
hridyanshmeena <43299684+hridyanshmeena@users.noreply.github.com>
2025-10-28 22:22:36 +0530

    Update login.js

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -12,7 +12,7 @@ export default function Login() {
     e.preventDefault();
     setMsg("Logging in...");
     try {
-      const res = await fetch("${process.env.NEXT_PUBLIC_API_BASE}/login", {
+      const res = await fetch("https://sqlwebapp.onrender.com/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password })
